54468212d8d223a88c6e7fcc18ddb9056ca71206
Wasim <er.wasim339@gmail.com>
2023-06-07 12:04:53 +0530

    changes

--- server/src/app.js
+++ server/src/app.js
@@ -1,25 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-dotenv.config();
+require("dotenv").config();
+
+
 
 const userRoutes = require('./routes/userRoutes')
 
 const app = express();
-// const PORT = process.env || 8000;
-
-
-// "mongodb://127.0.0.1:27017/student"   
-
-MONGODB_URL = "mongodb://127.0.0.1:27017/student"
 
 // middleware 
 app.use(cors());
 app.use(express.json());
 
 
-mongoose.connect(MONGODB_URL).then(()=>{console.log("Connected to DB")
+mongoose.connect("mongodb://127.0.0.1:27017/student").then(()=>{console.log("Connected to DB")
 
 console.log("hi")}).catch((err)=>console.log("Db not Connected"))
 
